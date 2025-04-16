9f8cd34ccde14b4cf94783a488945952c6f99bba
krisgwynelan <elan.krisgwyn08@gmail.com>
2025-04-16 23:06:35 +0800

    fullstack todo

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+from fastapi import HTTPException
 
 from . import models
 from . import schemas
@@ -7,7 +8,10 @@ def get_all_todos(db: Session):
     return db.query(models.ToDo).all()
 
 def get_todo(db: Session, todo_id: int):
-    return db.query(models.ToDo).filter(models.ToDo.id == todo_id).first()
+    todo = db.query(models.ToDo).filter(models.ToDo.id == todo_id).first()
+    if not todo:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return todo
 
 def create_todo(db: Session, todo: schemas.ToDoCreate):
     db_todo = models.ToDo(**todo.dict())
@@ -18,16 +22,17 @@ def create_todo(db: Session, todo: schemas.ToDoCreate):
 
 def update_todo(db: Session, todo_id: int, todo: schemas.ToDoUpdate):
     db_todo = get_todo(db, todo_id)
-    if db_todo:
-        for key, value in todo.dict().items():
-            setattr(db_todo, key, value)
-        db.commit()
-        db.refresh(db_todo)
+    for key, value in todo.dict(exclude_unset=True).items():
+        setattr(db_todo, key, value)
+    db.commit()
+    db.refresh(db_todo)
     return db_todo
 
 def delete_todo(db: Session, todo_id: int):
     db_todo = get_todo(db, todo_id)
-    if db_todo:
-        db.delete(db_todo)
-        db.commit()
-    return db_todo
+    db.delete(db_todo)
+    db.commit()
+    return {"message": f"Todo with ID {todo_id} deleted successfully."}
+
+def get_todos_by_status(db: Session, completed: bool):
+    return db.query(models.ToDo).filter(models.ToDo.completed == completed).all()
